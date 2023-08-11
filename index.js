bf20e95c99e356bf1a96a528a0e1bfe6190b3728
abdellatif tijani <tijani.idrissi.abdellatif@gmail.com>
2023-08-11 14:52:36 +0100

    cart and toggle

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ import fetchProducts from './src/fetchProducts.js';
 import { setupStore, store } from './src/store.js';
 import display from './src/displayProducts.js';
 import { getElement } from './src/utils.js';
+
+const init = async () => {
+    const products = await fetchProducts();
+    if (products) {
+      // add products to the store
+      setupStore(products);
+    }
+  };
+  
+  window.addEventListener('DOMContentLoaded', init);
+  
--- src/utils.js
+++ src/utils.js
@@ -15,9 +15,19 @@ const getElement = (selection) => {
 }
 
 const formatPrice = () => {}
+const getStorageItem = (item) => {
+  let storageItem = localStorage.getItem(item);
+  if (storageItem) {
+    storageItem = JSON.parse(localStorage.getItem(item));
+  } else {
+    storageItem = [];
+  }
+  return storageItem;
+};
 
-const getStorageItem = () => {}
-const setStorageItem = () => {}
+const setStorageItem = (name, item) => {
+  localStorage.setItem(name, JSON.stringify(item));
+};
 
 export {
   allProductsUrl,
