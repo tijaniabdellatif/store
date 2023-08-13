a36e17e5a00ed5fb207a9256d783309950ee9312
abdellatif tijani <tijani.idrissi.abdellatif@gmail.com>
2023-08-13 12:45:30 +0100

    update total update amount and set cart functionnalities

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -36,19 +36,81 @@ export const addToCart = (id) => {
 
     else {
 
-        /** update the cart */
+        const amount = increaseAmount(id);
+        const items = cartItemsDOM.querySelectorAll('.cart-item-amount');
+
+        console.log(items);
     }
 
-     
+      
+    displayCartItemCount();
+    displayCartTotal();
+    setStorageItem('cart',cart);
       
 
       openCart();
       
 };
 
+
+function displayCartItemCount(){
+
+   const amount = cart.reduce((total,item) => {
+
+            return (total += item.amount)
+   },0);
+
+    cartItemCountDOM.textContent = amount;
+}
+
+function displayCartTotal(){
+
+     let total = cart.reduce((acc,item) => {
+
+            return (acc += item.price * item.amount)
+
+     },0)
+
+    cartTotalDOM.textContent = `Total : ${formatPrice(total)}`;
+
+}
+function displayCartItemsDOM(){
+
+    cart.forEach(item => {
+        addToCartDOM(item);
+    })
+}
+
+  function increaseAmount(id){
+
+      let newAmount;
+
+        cart = cart.map((item) => {
+
+              if(item.id === parseInt(id)){
+                newAmount = item.amount + 1
+                    item = {...item,amount:newAmount}
+              }
+
+              return item;
+        })
+
+        return newAmount;
+  }
+
+function setupCartProcess(){
+
+
+}
 const init = () => {
 
-    console.log(cart);
+     displayCartItemCount();
+     displayCartTotal();
+     displayCartItemsDOM();
+     /**Initialize the dom with the cart */
+
+     setupCartProcess()
+
 }
 
 init();
