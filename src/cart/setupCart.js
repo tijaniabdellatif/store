aff5f16d6bfd1126770f7fb4f7cb7aa48331f12c
abdellatif tijani <tijani.idrissi.abdellatif@gmail.com>
2023-08-13 01:30:46 +0100

    setup cart functionnalities

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -10,10 +10,24 @@ import { findProduct } from '../store.js';
 import addToCartDOM from './addToCartDOM.js';
 // set items
 
+
+const cartItemCountDOM = getElement('.cart-item-count');
+const cartItemsDOM = getElement('.cart-items');
+const cartTotalDOM = getElement('.cart-total');
+
+let cart = getStorageItem('cart');
+
+
 export const addToCart = (id) => {
 
       console.log('add to cart');
-
-
+      openCart();
       
 };
+
+const init = () => {
+
+    console.log(cart);
+}
+
+init();
